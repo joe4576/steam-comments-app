4e0f4c4e2bd0567b3fce9b8caa8191ce1edd008c
Joe Brown <joebrown.4576@gmail.com>
2022-01-02 21:41:49 +0000

    Router init and create separate main search component

---
+++ frontend/steam-comments/src/router/router.ts
@@ -0,0 +1,32 @@
+import Vue from "vue";
+import VueRouter, { Route, RouteConfig } from "vue-router";
+import App from "@/App.vue";
+import { toRef } from "@vue/composition-api";
+import Search from "@/components/pages/Search.vue";
+
+Vue.use(VueRouter);
+
+const routes: RouteConfig[] = [
+  {
+    path: "/",
+    component: App,
+    name: "home",
+    redirect: "/search",
+  },
+  {
+    path: "/search",
+    component: Search,
+    name: "seacrh",
+  },
+];
+
+const router = new VueRouter({ routes, mode: "history" });
+
+export default router;
+
+export const useRouter = () => {
+  return {
+    router,
+    route: toRef(router, "currentRoute"),
+  };
+};
